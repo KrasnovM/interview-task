30fae69042c740fb01a0a5249dd75de7b40a0e28
KrasnovM <krasnov.m2000@gmail.com>
2024-05-08 16:37:54 +0300

    created ProductService

---
+++ src/main/java/ru/krasnovm/interviewtask/service/ProductService.java
@@ -0,0 +1,66 @@
+package ru.krasnovm.interviewtask.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.krasnovm.interviewtask.entity.Product;
+import ru.krasnovm.interviewtask.repository.ProductCollection;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private final ProductCollection productRepository;
+
+    public ProductService(ProductCollection productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product create(Product product) {
+        if (!validateProduct(product)) {
+            return null;
+        }
+
+        return productRepository.create(product);
+    }
+
+    public List<Product> readAll() {
+        return productRepository.readAll();
+    }
+
+    public Product readByName(String name) {
+        return productRepository.readByName(name);
+    }
+
+    public Product update(Product product) {
+        if (!validateProduct(product)) {
+            return null;
+        }
+
+        return productRepository.update(product);
+    }
+
+    public void delete (Product product) {
+        productRepository.delete(product);
+    }
+
+    private boolean validateProduct(Product product) {
+        //Название товара ограничено 255 символами и оно обязательно при создании
+        if (product.getName().length() > 255 ||
+                product.getName().isEmpty()) {
+            return false;
+        }
+
+        //Описание товара ограничено 4096 символами
+        if (product.getDescription().length() > 4096) {
+            return false;
+        }
+
+        //Цена товара не может быть меньше 0
+        if (product.getPrice() < 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
