b72c441154a9897f80543e17dacb4cc8b379b9a8
KrasnovM <krasnov.m2000@gmail.com>
2024-05-09 15:59:13 +0300

    created ProductController

---
+++ src/main/java/ru/krasnovm/interviewtask/controller/ProductController.java
@@ -0,0 +1,55 @@
+package ru.krasnovm.interviewtask.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.krasnovm.interviewtask.entity.Product;
+import ru.krasnovm.interviewtask.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    private ResponseEntity<Product> mappingResponseEntity(Product product) {
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    private ResponseEntity<List<Product>> mappingResponseEntityList(List<Product> products) {
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return mappingResponseEntity(productService.create(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> readAll() {
+        return mappingResponseEntityList(productService.readAll());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<List<Product>> readByCategoryId(@PathVariable("name") String name) {
+        return mappingResponseEntityList(productService.readByName(name));
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody Product product) {
+        return mappingResponseEntity(productService.update(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus delete(@PathVariable("id") Long id) {
+        productService.delete(id);
+        return HttpStatus.OK;
+    }
+}
