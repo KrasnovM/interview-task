c54f760ef71a46768a827f66c5ec53722a0ee8db
KrasnovM <krasnov.m2000@gmail.com>
2024-05-09 18:13:33 +0300

    excluded DataSourceAutoConfiguration class to start app without db connection

--- src/main/java/ru/krasnovm/interviewtask/InterviewTaskApplication.java
+++ src/main/java/ru/krasnovm/interviewtask/InterviewTaskApplication.java
@@ -2,8 +2,9 @@ package ru.krasnovm.interviewtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InterviewTaskApplication {
 
 	public static void main(String[] args) {
