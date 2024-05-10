8fd129d5d54e27a19742bf6752eb79cc3d35dbcb
KrasnovM <krasnov.m2000@gmail.com>
2024-05-10 09:05:07 +0300

    created schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS product (
   name          VARCHAR(255) NOT NULL,
   description   VARCHAR(4096) NOT NULL,
   price         NUMERIC(8,2) NOT NULL,
-  in_stock       BOOLEAN NOT NULL,
+  in_stock     BOOLEAN NOT NULL,
   CONSTRAINT chk_price_positive CHECK (price>= 0)
   );
