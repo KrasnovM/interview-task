ddf5102953009aa0e353745d176d31b455e18fcb
KrasnovM <krasnov.m2000@gmail.com>
2024-05-10 06:07:38 +0300

    added default schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS product (
+  id            BIGSERIAL PRIMARY KEY,
+  name          VARCHAR(255) NOT NULL,
+  description   VARCHAR(4096) NOT NULL,
+  price         NUMERIC(8,2) NOT NULL,
+  inStock       BOOLEAN NOT NULL,
+  CONSTRAINT chk_price_positive CHECK (price>= 0)
+  );
