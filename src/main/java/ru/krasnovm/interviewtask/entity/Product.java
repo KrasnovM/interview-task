e4f34f2f89b6e3f890596ad5febfb0fca902c267
KrasnovM <krasnov.m2000@gmail.com>
2024-05-10 09:05:49 +0300

    made constructor public for jpa

--- src/main/java/ru/krasnovm/interviewtask/entity/Product.java
+++ src/main/java/ru/krasnovm/interviewtask/entity/Product.java
@@ -19,7 +19,7 @@ public class Product {
     public Product() {
     }
 
-    private Product(Long id, String name, String description, Double price, Boolean inStock) {
+    public Product(Long id, String name, String description, Double price, Boolean inStock) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -97,6 +97,26 @@ public class Product {
 
     public Boolean getInStock() {
         return inStock;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setInStock(Boolean inStock) {
+        this.inStock = inStock;
     }
 
     @Override
